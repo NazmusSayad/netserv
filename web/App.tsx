a63a7d61b4a3bb3cadb0d85f3d79da465262a921
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-07-25 12:17:18 +0600

    auth setup

--- web/App.tsx
+++ web/App.tsx
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { createSuspense } from './api/react'
 import { actions } from './store'
 import Router from './Router'
+import { Suspense } from 'react'
 
 const theme = createTheme({
   palette: {
@@ -10,21 +11,26 @@ const theme = createTheme({
   },
 })
 
-const useSuspense = createSuspense()
-
 function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AppInit />
+      </Suspense>
+    </ThemeProvider>
+  )
+}
+
+const useSuspense = createSuspense()
+function AppInit() {
   useSuspense({ url: '/auth/init' }, ([{ data }]) => {
     if (data?.jwt) actions.auth.login(data?.jwt)
     else if (data?.authEnabled) actions.auth.enableAuth()
     else throw new Error('No auth or auth enabled')
   })
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router />
-    </ThemeProvider>
-  )
+  return <Router />
 }
 
 export default App
--- web/features/Auth/Login.tsx
+++ web/features/Auth/Login.tsx
@@ -1,22 +1,29 @@
-import { FormEvent, useState } from 'react'
-import css from './Login.module.scss'
 import { Button, Input, ButtonBase } from '@mui/material'
-import { actions } from '@/store'
-import { MdPerson } from 'react-icons/md'
-import { useApi } from '@/api/react'
+import { FormEvent, Suspense, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { MdPerson } from 'react-icons/md'
+import { FaRegEyeSlash, FaRegEye } from 'react-icons/fa'
 
-export default function Login() {
+import { useApi } from '@/api/react'
+import { actions } from '@/store'
+import css from './Login.module.scss'
+
+function LoginForm() {
   const [isPassVisible, setIsPassVisible] = useState(false)
+  const [apiError, setApiError] = useState('')
   const navigate = useNavigate()
-  const api = useApi()
+  const api = useApi({ suspense: true })
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault()
-    const password: string = e.target['password'].value
+    setApiError('')
 
-    const { ok, data } = await api.post('/auth/login', { password })
-    if (!ok) return
+    const password: string = e.target['password'].value
+    const { ok, data, error } = await api.post('/auth/login', { password })
+
+    if (!ok) {
+      return setApiError(error)
+    }
 
     if (data.jwt) {
       navigate('/')
@@ -25,35 +32,48 @@ export default function Login() {
   }
 
   return (
+    <form onSubmit={handleSubmit}>
+      <div className={css.logo}>
+        <MdPerson />
+      </div>
+
+      <div className={css.inputContainer}>
+        <Input
+          fullWidth
+          name="password"
+          className={css.input}
+          type={isPassVisible ? 'text' : 'password'}
+          onChange={() => setApiError('')}
+        />
+
+        <div className={css.passwordToggleVisible}>
+          <ButtonBase
+            type="button"
+            onClick={() => setIsPassVisible((prev) => !prev)}
+          >
+            {isPassVisible ? <FaRegEyeSlash /> : <FaRegEye />}
+          </ButtonBase>
+        </div>
+
+        {apiError && <p className={css.error}>{apiError}</p>}
+      </div>
+
+      <Button type="submit" variant="outlined" fullWidth>
+        login
+      </Button>
+    </form>
+  )
+}
+
+export default function Login() {
+  return (
     <div className={css.CardContainer}>
       <div className={css.Card}>
-        <form onSubmit={handleSubmit}>
-          <div className={css.logo}>
-            <MdPerson />
-          </div>
-
-          <div className={css.inputContainer}>
-            <Input
-              fullWidth
-              name="password"
-              className={css.input}
-              type={isPassVisible ? 'text' : 'password'}
-            />
-
-            <div className={css.passwordToggleVisible}>
-              <ButtonBase
-                type="submit"
-                onClick={() => setIsPassVisible((prev) => !prev)}
-              >
-                ++
-              </ButtonBase>
-            </div>
-          </div>
-
-          <Button type="submit" variant="outlined" fullWidth>
-            login
-          </Button>
-        </form>
+        <Suspense
+          fallback={<h1 style={{ textAlign: 'center' }}>Logging in...</h1>}
+        >
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   )
