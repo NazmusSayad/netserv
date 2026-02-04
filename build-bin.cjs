820091d14b7b4f0630f4499c6da25f9b27bc946d
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-05 02:39:41 +0600

    Update package version to 3.0.19, add netserv.exe to .gitignore, and mark several dependencies as peer in package-lock.json

---
+++ build-bin.cjs
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const spawn = require('cross-spawn')
+const packageJson = require('./package.json')
+
+const dirsToDelete = ['dist', 'dist-web']
+dirsToDelete.forEach((dir) => {
+  if (fs.existsSync(dir)) {
+    console.log(`Deleting "${dir}" folder...`)
+    fs.rmSync(path.resolve(__dirname, dir), { recursive: true })
+  }
+})
+
+console.log('Starting building...')
+for (const key in packageJson.scripts) {
+  if (key.startsWith('build-')) {
+    spawn.sync(`npm run ${key}`, [], {
+      stdio: 'inherit',
+    })
+  }
+}
+
+spawn(
+  'bun',
+  [
+    'build',
+    './dist/server/index.js',
+    '--compile',
+    '--target',
+    'bun',
+    '--outfile',
+    'netserv.exe',
+  ],
+  { stdio: 'inherit' }
+)
